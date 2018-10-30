ca3325e392bfd947686beb9251f89ef4d07dc14e
Tanaka <python1120@yahoo.co.jp>
2018-10-30 23:02:38 +0900

    サーボモーターの実装

---
+++ servoMotor.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import time
+
+ARM_STATE = {
+    'OPEN': 0,
+    'CLOSE': 1
+}
+
+class ServoMotor():
+    def __init__(self, pin, state = 'OPEN', hz = 50):
+        self.pin = pin
+        GPIO.setmode( GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.servo = GPIO.PWM(self.pin, hz)
+        self.state = ARM_STATE[state]
+        self.servo.start(0)
+
+    def open(self):
+        if self.state == ARM_STATE['OPEN']:
+            return
+
+        self.servo.ChangeDutyCycle(2.5)
+        time.sleep(0.5)
+        self.state = ARM_STATE['OPEN']
+
+    def close(self):
+        if self.state == ARM_STATE['CLOSE']:
+            return
+
+        self.servo.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        self.state = ARM_STATE['CLOSE']
+
+    def stop(self):
+        self.servo.stop()
+
+if __name__ == '__main__':
+    servo = ServoMotor(18)
+    servo.servo.start(0)
+    print('init servo state %s' % servo.state)
+    servo.open()
+    print('open servo state %s' % servo.state)
+    servo.close()
+    print('close servo state %s' % servo.state)
+    servo.open()
+    print('open servo state %s' % servo.state)
+    servo.servo.stop()
+    GPIO.cleanup()
